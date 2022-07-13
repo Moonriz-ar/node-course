4015e08cfbb8c05a27d25a1504bdeb5797c92239
Andrea Lin <andre93.lin@gmail.com>
2022-07-13 10:43:47 -0300

    add checkBody middleware, learn how to chain middleware functions

--- 4-natours/starter/controllers/tourController.js
+++ 4-natours/starter/controllers/tourController.js
@@ -19,6 +19,17 @@ exports.checkID = (req, res, next, val) => {
   next();
 };
 
+// checkBody function
+exports.checkBody = (req, res, next) => {
+  if (!req.body.name || !req.body.price) {
+    return res.status(400).json({
+      status: 'fail',
+      message: 'Missing name or price',
+    });
+  }
+  next();
+};
+
 ////////////////////////////////////////////////////////
 // tours controllers functions
 
