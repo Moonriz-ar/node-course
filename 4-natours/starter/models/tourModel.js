f1019a80766ef6e7fc5afd4f5d23f5808f88aa84
Andrea Lin <andre93.lin@gmail.com>
2022-07-16 09:01:31 -0300

    Update tourSchema

--- 4-natours/starter/models/tourModel.js
+++ 4-natours/starter/models/tourModel.js
@@ -1,18 +1,55 @@
 const mongoose = require('mongoose');
 
 const tourSchema = new mongoose.Schema({
+  createdAt: {
+    default: Date.now(),
+    type: Date,
+  },
+  description: {
+    required: [true, 'A tour must have a description'],
+    trim: true,
+    type: String,
+  },
+  difficulty: {
+    required: [true, 'A tour must have a difficulty'],
+    type: String,
+  },
+  duration: {
+    required: [true, 'A tour must have a duration'],
+    type: Number,
+  },
+  imageCover: {
+    required: [true, 'A tour must have a cover image'],
+    type: String,
+  },
+  images: [String],
+  maxGroupSize: {
+    required: [true, 'A tour must have a group size'],
+    type: Number,
+  },
   name: {
     required: [true, 'A tour must have a name'],
+    trim: true,
     type: String,
     unique: true,
   },
-  rating: {
+  ratingsAverage: {
     default: 4.5,
+    type: Number,
+  },
+  ratingsQuantity: {
+    default: 0,
     type: Number,
   },
   price: {
     required: [true, 'A tour must have a price'],
     type: Number,
+  },
+  priceDiscount: Number,
+  startDates: [Date],
+  summary: {
+    trim: true,
+    type: String,
   },
 });
 
