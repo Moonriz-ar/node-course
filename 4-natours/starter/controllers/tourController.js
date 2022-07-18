d51382cd25e490fde21ebd9e0b36078392e73664
Andrea Lin <andre93.lin@gmail.com>
2022-07-18 15:45:55 -0300

    Add alias for top5 tours

--- 4-natours/starter/controllers/tourController.js
+++ 4-natours/starter/controllers/tourController.js
@@ -1,6 +1,15 @@
 const Tour = require('../models/tourModel');
 
 ////////////////////////////////////////////////////////
+// middleware
+exports.aliasTopTours = (req, res, next) => {
+  req.query.limit = '5';
+  req.query.sort = '-ratingsAverage,price';
+  req.query.fields = 'name,price,ratingsAverage,summary,difficulty';
+  next();
+};
+
+////////////////////////////////////////////////////////
 // tours controllers functions
 
 exports.getAllTours = async (req, res) => {
