c7900c1a40c1eb98599c52f19fb86c4666f24c2a
Andrea Lin <andre93.lin@gmail.com>
2022-08-01 15:05:48 -0300

    Add validations

--- 4-natours/starter/models/tourModel.js
+++ 4-natours/starter/models/tourModel.js
@@ -1,5 +1,6 @@
 const mongoose = require('mongoose');
 const slugify = require('slugify');
+const validator = require('validator');
 
 const tourSchema = new mongoose.Schema(
   {
@@ -13,6 +14,10 @@ const tourSchema = new mongoose.Schema(
       type: String,
     },
     difficulty: {
+      enum: {
+        values: ['easy', 'medium', 'difficult'],
+        message: 'Difficulty is either: easy, medium, difficult',
+      },
       required: [true, 'A tour must have a difficulty'],
       type: String,
     },
@@ -30,13 +35,18 @@ const tourSchema = new mongoose.Schema(
       type: Number,
     },
     name: {
+      maxlength: [40, 'A tour name must have less or equal than 40 characters'],
+      minlength: [10, 'A tour name must have more or equal than 10 characters'],
       required: [true, 'A tour must have a name'],
       trim: true,
       type: String,
       unique: true,
+      // validate: [validator.isAlpha, 'Tour name must only contain characters'],
     },
     ratingsAverage: {
       default: 4.5,
+      min: [1, 'Rating must be above 1.0'],
+      max: [5, 'Rating must be below 5.0'],
       type: Number,
     },
     ratingsQuantity: {
@@ -47,7 +57,16 @@ const tourSchema = new mongoose.Schema(
       required: [true, 'A tour must have a price'],
       type: Number,
     },
-    priceDiscount: Number,
+    priceDiscount: {
+      type: Number,
+      validate: {
+        message: 'Discount price should be below regular price',
+        validator: function (value) {
+          // this only points to current doc on NEW document creation
+          return value < this.price; // return false if the discounted price is higher than price. returning false indicates a validation error
+        },
+      },
+    },
     secretTour: {
       type: Boolean,
       default: false,
