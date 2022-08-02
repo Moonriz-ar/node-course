d1349179948d12ca48e6d8b8bd7cbcb979107fe7
Andrea Lin <andre93.lin@gmail.com>
2022-08-02 15:49:02 -0300

    Add global error handling middleware

--- 4-natours/starter/app.js
+++ 4-natours/starter/app.js
@@ -33,9 +33,22 @@ app.use('/api/v1/tours', tourRouter);
 app.use('/api/v1/users', userRouter);
 
 app.all('*', (req, res, next) => {
-  res.status(404).json({
-    status: 'fail',
-    message: `Can't find ${req.originalUrl} on this server!`,
+  const err = new Error(`Can't find ${req.originalUrl} on this server!`);
+  err.status = 'fail';
+  err.statusCode = 404;
+
+  // whenever we pass an argument inside next function, it will assume there was an error, skip the next middlewares and send the err object to the global error handling middleware
+  next(err);
+});
+
+// middleware for global error handling
+app.use((err, req, res, next) => {
+  err.statusCode = err.statusCode || 500;
+  err.status = err.status || 'error';
+
+  res.status(err.statusCode).json({
+    status: err.status,
+    message: err.message,
   });
 });
 
